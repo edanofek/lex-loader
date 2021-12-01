bc0922e9b0f74acc190d958b280c10881126092b
idan.o <idan.o@solaredge.com>
2021-12-01 12:26:27 +0200

    add support process.env.PORT

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-// app.use(express.static('public'))
-// app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname))
 
 app.get('/',(req,res)=>{
     return res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.listen(port,()=>{
+app.listen(process.env.PORT || port,()=>{
     console.log("working on "+port)
 });
